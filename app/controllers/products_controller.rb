ef96011b05abd2213a1a419da0f4e6271dd056e5
alycesuth <alycesuth@comcast.net>
2025-02-19 13:16:59 -0600

    Added routes and fixed index and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def all_products_method
+    @products = Product.all
+    render template: "products/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/available_products", controller: "products", action: "all_products_method"
 end
