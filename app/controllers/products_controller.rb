f37c12ec1816a43d42a4bddf4d10d82e9ef20e7a
alycesuth <alycesuth@comcast.net>
2025-02-20 12:08:41 -0600

    added create method and route, added barebones of new, edit, update, destroy routes and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,28 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    render :show
+  end
+
+  def new
+    render json: "new"
+  end
+
+  def edit
+    render json: "edit"
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   # root "posts#index"
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  get "/products/new" => "products#new"
+  get "/products/:id/edit" => "products#edit"
+  put "/prodcts/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
