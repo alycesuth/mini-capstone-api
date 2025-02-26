6a59684bb8161f562bb345db1b1059401c5da7e8
alycesuth <alycesuth@comcast.net>
2025-02-26 14:18:16 -0600

    added index and show methods, routes, and also addec controller and jbuilder for suppliers

---
+++ app/views/suppliers/_supplier.json.jbuilder
@@ -0,0 +1,6 @@
+json.id supplier.id
+json.name supplier.name
+json.email supplier.email
+json.phoneNumber supplier.phone_number
+json.createdAt supplier.created_at
+json.updatedAt supplier.updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_193813) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_26_200158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,5 +22,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_26_193813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
