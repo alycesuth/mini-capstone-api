a78a46393033016293fd1c3467e2cd6f072b0d98
alycesuth <alycesuth@comcast.net>
2025-03-18 15:01:53 -0500

    changed show method to require coookie

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    @order = current_user.orders.find_by(id: params[:id])
     render :show
   end
 end
