55138764afd529f42858e798498673696f6c5cd9
alycesuth <alycesuth@comcast.net>
2025-04-03 11:36:56 -0500

    Fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,100 +8,89 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# Product.create(
-#   name: "Sex Pistols Sticker",
-#   price: 1.0,
-#   description: "A 3-inch sticker of the Sex Pistols logo in pink and yellow.",
-#   supplier_id: 2
-# )
 
-# Product.create(
-#   name: "Dead Kennedys Sticker",
-#   price: 2.0,
-#   description: "A 2-inch vinyl sticker of the Dead Kennedys logo.",
-#   supplier_id: 2
-# )
+Product.create(
+  name: "Sex Pistols Sticker",
+  price: 1.0,
+  description: "A 3-inch sticker of the Sex Pistols logo in pink and yellow.",
+  supplier_id: 2
+)
 
-# Product.create(
-#   name: "Descendents Sticker",
-#   price: 3.0,
-#   description: "An 8-inch vinyl sticker of the punk band Descendents featuring Milo",
-#   supplier_id: 1
-# )
+Product.create(
+  name: "Dead Kennedys Sticker",
+  price: 2.0,
+  description: "A 2-inch vinyl sticker of the Dead Kennedys logo.",
+  supplier_id: 2
+)
 
-# Product.create(
-#   name: "Black Flag Sticker",
-#   price: 1.0,
-#   description: "A 3-inch vinyl sticker of the band Black Flag's logo.",
-#   supplier_id: 1
-# )
+Product.create(
+  name: "Descendents Sticker",
+  price: 3.0,
+  description: "An 8-inch vinyl sticker of the punk band Descendents featuring Milo",
+  supplier_id: 1
+)
 
-# Product.create(
-#   name: "Misfits Sticker",
-#   price: 2.0,
-#   description: "A 3-inch vinyl sticker of the Crimson Ghost Misfits logo.",
-#   supplier_id: 1
-# )
+Product.create(
+  name: "Black Flag Sticker",
+  price: 1.0,
+  description: "A 3-inch vinyl sticker of the band Black Flag's logo.",
+  supplier_id: 1
+)
 
-# Supplier.create(
-#   name: "Hot Topic",
-#   email: "hottopic@gmail.com",
-#   phone_number: "6458769000"
-# )
-
-# Supplier.create(
-#   name: "Books A Million",
-#   email: "bam@gmail.com",
-#   phone_number: "800987900"
-# )
+Product.create(
+  name: "Misfits Sticker",
+  price: 2.0,
+  description: "A 3-inch vinyl sticker of the Crimson Ghost Misfits logo.",
+  supplier_id: 1
+)
 
 Image.create(
   product_id: 1,
+  url: "https://i.ebayimg.com/images/g/xN8AAOSwBkNb-HhA/s-l1200.jpg"
+)
+
+Image.create(
+  product_id: 1,
+  url: "https://i.etsystatic.com/10170765/r/il/a971cb/1695202882/il_fullxfull.1695202882_k0v9.jpg"
+)
+
+Image.create(
+  product_id: 2,
+  url: "https://m.media-amazon.com/images/I/41y+fo2cY+L._AC_UF894,1000_QL80_.jpg"
+)
+
+Image.create(
+  product_id: 2,
+  url: "https://www.stogieswv.com/cdn/shop/products/pixelcut-export-1671216554833.jpg?v=1671645142&width=1946"
+)
+
+Image.create(
+  product_id: 3,
+  url: "https://m.media-amazon.com/images/I/61LPRMNVJhL.jpg"
+)
+
+Image.create(
+  product_id: 3,
+  url: "https://render.fineartamerica.com/images/rendered/default/surface/sticker/images/artworkimages/medium/3/descendents-official-merchandise-everything-sucks-oleg-aster-transparent.png?&targetx=62&targety=0&imagewidth=875&imageheight=1000&modelwidth=1000&modelheight=1000&backgroundcolor=646464&stickerbackgroundcolor=transparent&orientation=0&producttype=sticker-3-3&v=8"
+)
+
+Image.create(
+  product_id: 4,
+  url: "https://sstsuperstore.com/cdn/shop/products/SSTS15.jpg?v=1632148061"
+)
+
+Image.create(
+  product_id: 4,
+  url: "https://headlinerecords.com/cdn/shop/products/21STICKER.jpg?v=1738206652"
+)
+
+Image.create(
+  product_id: 5,
   url: "https://headlinerecords.com/cdn/shop/products/799_281_29.jpg?v=1738202272"
 )
 
 
 Image.create(
-  product_id: 1,
+  product_id: 5,
   url: "https://res.cloudinary.com/teepublic/image/private/s--cDX0ewNu--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1652636752/production/designs/30345012_0.jpg"
-)
-
-Image.create(
-  product_id: 6,
-  url: "https://i.ebayimg.com/images/g/xN8AAOSwBkNb-HhA/s-l1200.jpg"
-)
-
-Image.create(
-  product_id: 6,
-  url: "https://i.etsystatic.com/10170765/r/il/a971cb/1695202882/il_fullxfull.1695202882_k0v9.jpg"
-)
-
-Image.create(
-  product_id: 7,
-  url: "https://m.media-amazon.com/images/I/41y+fo2cY+L._AC_UF894,1000_QL80_.jpg"
-)
-
-Image.create(
-  product_id: 7,
-  url: "https://www.stogieswv.com/cdn/shop/products/pixelcut-export-1671216554833.jpg?v=1671645142&width=1946"
-)
-
-Image.create(
-  product_id: 2,
-  url: "https://m.media-amazon.com/images/I/61LPRMNVJhL.jpg"
-)
-
-Image.create(
-  product_id: 2,
-  url: "https://render.fineartamerica.com/images/rendered/default/surface/sticker/images/artworkimages/medium/3/descendents-official-merchandise-everything-sucks-oleg-aster-transparent.png?&targetx=62&targety=0&imagewidth=875&imageheight=1000&modelwidth=1000&modelheight=1000&backgroundcolor=646464&stickerbackgroundcolor=transparent&orientation=0&producttype=sticker-3-3&v=8"
-)
-
-Image.create(
-  product_id: 3,
-  url: "https://sstsuperstore.com/cdn/shop/products/SSTS15.jpg?v=1632148061"
-)
-
-Image.create(
-  product_id: 3,
-  url: "https://headlinerecords.com/cdn/shop/products/21STICKER.jpg?v=1738206652"
 )
